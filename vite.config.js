d7631cfb83fc44e5cf3b2bea6874922186451e88
Thimira141 <Thimira141@github.com>
2026-07-28 02:07:20 +0530

    feat: install data table via npm

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js',
-                'resources/js/students.js'
+                'resources/js/students.js',
+                'resources/css/dt-imports.css',
             ],
             refresh: true,
         }),
     ],
+    build: {
+    // Raise the warning threshold (default is 500 KB)
+    chunkSizeWarningLimit: 1000,
+  },
 });
