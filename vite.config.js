1cb25914a78c0ee390b18c57a0c56687b6ca1876
Thimira141 <Thimira141@github.com>
2026-08-01 07:13:05 +0530

    feat: add js to config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js',
-                'resources/js/students.js',
+                'resources/js/students.js', 'resources/js/guardians.js',
                 'resources/css/dt-imports.css',
             ],
             refresh: true,
