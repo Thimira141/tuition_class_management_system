e047595f9377e1e1d0479c79d741c6dd4a3255f3
Thimira141 <Thimira141@github.com>
2026-08-02 23:26:51 +0530

    feat: enhance guardians management with data table and modal functionalities

--- resources/js/guardians.js
+++ resources/js/guardians.js
@@ -1,17 +1,33 @@
 import { clearFormErrors } from "./ajax";
-import { load_data, setFieldValue, cleanupModalBackdrop, createBootstrapModal } from "./utility";
+import { load_data, setFieldValue, cleanupModalBackdrop, createBootstrapModal, DT_handleDeleteRestore, DT_setDeletedState, dt_show_deleted_records } from "./utility";
+import { renderProfileRow } from './ui/dt-render-profile-row';
+import { createActionRow } from "./ui/common";
+
+/**
+ * DT imports
+ */
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-buttons-bs5';
+import 'datatables.net-select-bs5';
 
 // identifiers NEW GUARDIAN modal
 const GUARDIAN_NEW_MODAL = '#new-guardian-model';
 const GUARDIAN_NEW_MODAL_TRIGGER = '#new-guardian-add-model-init-btn';
 const GUARDIAN_NEW_MODAL_FORM = '#new-edit-guardian-form';
+const GUARDIAN_VIEW_MODAL = '#view-guardian-model';
+
+// data table
+const GUARDIAN_DT_INDEX_TABLE = '#dt-guardians-index-table'; // table
 
 // variables
+let DTable;
 
 // inits
 document.addEventListener('DOMContentLoaded', () => {
-    triggerNewGuardianModal();
+    // triggerNewGuardianModal();
     formEventsTracker();
+    // init dt
+    DTable = initIndexDT();
 });
 
 // reusable functions
@@ -104,14 +120,143 @@ async function loadGuardianData(guardianCode, form, modal) {
     for (const [key, value] of Object.entries(response.user)) {
         modal.querySelectorAll(`[name="${key}"], #${key}`).forEach(field => {
             setFieldValue(field, value);
-            // attach function to show/hide new-edit modal -> form -> staff_deps select
-            // const _child = document.querySelector(field.getAttribute('data-child'));
-            // const _wrapper = document.querySelector(field.getAttribute('data-wrapper'));
-            // if (_child || _wrapper) {
-            //     toggleChildVisibilityByParentValue(_child, _wrapper, value); // todo run tests
-            // }
         });
+        // setup cover image
+        const coverImgPreview = modal.querySelector('img#guardian__cover_img-preview');
+        if (coverImgPreview) coverImgPreview.src = guardian.guardian__cover_img;
     }
 
     return response;
 }
+
+/**
+ * open view modal and assign data to its fields
+ * @param {string} guardianCode
+ */
+async function openGuardianViewModal(guardianCode) {
+    const ModalElement = document.querySelector(GUARDIAN_VIEW_MODAL);
+    const viewModal = createBootstrapModal(ModalElement);
+
+    if (!ModalElement || !viewModal) {
+        console.error('Guardian view modal elements not found');
+        return null;
+    }
+
+    // load data
+    const response = await load_data(ROUTES.guardians_show.replace(':guardian_code', guardianCode), GUARDIAN_VIEW_MODAL);
+
+    const guardian = response?.guardian || response?.data?.guardian || {};
+    if (!guardian) {
+        console.log('Error Loading Data for guardian view modal');
+        return false;
+    }
+    // guardian data fill
+    for (const [key, value] of Object.entries(guardian)) {
+        ModalElement.querySelectorAll(`[name="${key}"], #${key}`).forEach(field => {
+            setFieldValue(field, value);
+        });
+    }
+    // todo: students list data fill
+    // setup cover image
+    const coverImgPreview = ModalElement.querySelector('img#guardian__cover_img-preview');
+    if (coverImgPreview) coverImgPreview.src = guardian.guardian__cover_img;
+
+    // open modal
+    cleanupModalBackdrop();
+    viewModal.show();
+}
+
+/**
+ * data table
+ */
+function initIndexDT() {
+    const table = new DataTable(GUARDIAN_DT_INDEX_TABLE, {
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: ROUTES.guardians_dt_index,
+            data: (d) => {
+                d.showDeleted = dt_show_deleted_records; // custom param
+            }
+        },
+        columns: [
+            { data: null, defaultContent: '', className: 'select-checkbox', orderable: false },
+            {
+                data: 'guardian__name',
+                name: 'guardians.name',
+                orderable: true, searchable: true,
+                render: (_, __, row) => renderProfileRow(
+                    row.guardian__cover_img,
+                    [row.guardian__name, '#' + row.guardian__guardian_code]
+                )
+            },
+            { data: 'guardian__tel', name: 'guardians.tel', orderable: true, searchable: true },
+            { data: 'guardian__nic', name: 'nic', orderable: true, searchable: true },
+        ],
+        layout: {
+            topStart: {
+                buttons: [
+                    { text: 'New', className: 'btn-primary', action: () => setupGuardianModal('new') },
+                    {
+                        text: 'View', className: 'btn-info dt-action-btn', action: () => {
+                            const selected = table.rows({ selected: true }).data();
+                            if (selected.length) openGuardianViewModal(selected[0].guardian__guardian_code);
+                        }
+                    },
+                    {
+                        text: 'Edit', className: 'btn-info dt-action-btn', action: () => {
+                            const selected = table.rows({ selected: true }).data();
+                            if (selected.length) setupGuardianModal('edit', selected[0].guardian__guardian_code);
+                        }
+                    },
+                    {
+                        text: 'Delete',
+                        className: 'btn-danger btn-delete-restore dt-action-btn',
+                        action: () => {
+                            const selected = table.rows({ selected: true }).data();
+                            if (selected.length) {
+                                handleDeleteRestore(selected[0], table);
+                            }
+                        }
+                    },
+                    {
+                        extend: 'collection',
+                        text: 'Records Filter',
+                        buttons: [
+                            {
+                                text: 'Active Records',
+                                className: 'buttons-active',
+                                action: () => DT_setDeletedState('active', table)
+                            },
+                            {
+                                text: 'Deleted Records',
+                                className: 'buttons-deleted',
+                                action: () => DT_setDeletedState('deleted', table)
+                            }
+                        ]
+                    },
+                ]
+            }
+        },
+        select: {
+            style: 'single',
+            selector: 'td.select-checkbox'
+        },
+        order: [[1, 'asc']]
+    });
+
+    return table;
+}
+
+/**
+ * submit delete form guardians
+ * @param {object} row - row data
+ * @param {Object} table - data table
+ */
+function handleDeleteRestore(row, table) {
+    const actionText = row.guardian__is_deleted ? "Restore" : "Delete";
+    const message = `Confirm ${actionText} guardian: ${row.guardian__name}?`;
+    const action = ROUTES.guardians_destroy.replace(':guardian_code', row.guardian__guardian_code);
+    DT_handleDeleteRestore(table, action, message)
+}
+
