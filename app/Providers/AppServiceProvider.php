781448ea38f7c3fa2f775cb6828179ebcbe31da6
Thimira141 <Thimira141@github.com>
2026-08-06 23:11:12 +0530

    feat: Bind classroom by class_code and include trashed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Classroom;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        // Bind classroom by class_code and include trashed
+        Route::bind('classroom', fn ($value) =>  Classroom::withTrashed()->where(Classroom::getRouteKeyName(), $value)->firstOrFail());
     }
 }
