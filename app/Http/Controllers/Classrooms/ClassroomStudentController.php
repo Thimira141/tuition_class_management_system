f8b54188a4b2dc4de60bb8614c50550ace8f8054
Thimira141 <Thimira141@github.com>
2026-08-07 07:46:31 +0530

    fix: add missing cover image url field

--- app/Http/Controllers/Classrooms/ClassroomStudentController.php
+++ app/Http/Controllers/Classrooms/ClassroomStudentController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Classroom;
 use App\Models\Student;
-use App\Traits\StripsPrefixes;
+use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\Facades\DataTables;
 
 class ClassroomStudentController extends Controller
@@ -40,6 +40,7 @@ class ClassroomStudentController extends Controller
             'students.name as student__name',
             'students.student_code as student__student_code',
             'students.dob as student__dob',
+            'students.cover_img as student__cover_img',
         ]);
 
         // keep a memory for attached students
@@ -47,7 +48,9 @@ class ClassroomStudentController extends Controller
 
         return DataTables::of($query)
             // Check if this student is attached to the classroom from memory
-            ->addColumn('is_attached', fn($student) => \in_array($student->student__id, $attachedIds))
+            ->addColumn('student__is_attached', fn($student) => \in_array($student->student__id, $attachedIds))
+            // cover image url
+            ->addColumn('student__cover_img_url', fn ($student) => $student->student__cover_img ? Storage::url($student->student__cover_img) : asset('images/placeholder-image-member.svg'))
             ->make(true);
     }
 
